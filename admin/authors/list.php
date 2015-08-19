b66c8ed474d5ee660cbd751e762a4f6471a64dee
deltagel <deltagel@gmail.com>
2015-08-19 22:27:57 +0300

    minor changes in admin

--- admin/authors/list.php
+++ admin/authors/list.php
@@ -1,51 +1,56 @@
 <?php
-	//Model 
-	function getAllauthors() {
-		include("../../inc/dbconn.php");
-		
-		$sql_command = 'SELECT * FROM authors';
-		$result = mysqli_query($conn, $sql_command);
-		$authors = array();
-		while($row = mysqli_fetch_assoc($result)) {
-			$authors[] = $row;
-		}
-		mysqli_close($conn);
-		
-		return $authors;
-	}
+header('Content-Type: text/html; charset=utf-8');
+
+//Model 
+include_once("../../inc/dbconn.php");
+
+function getAllauthors() {
+
+    $conn = open_database_connection();
+
+    $sql_command = 'SELECT * FROM authors';
+    $result = mysqli_query($conn, $sql_command);
+    $authors = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $authors[] = $row;
+    }
+    close_database_connection($conn);
+
+    return $authors;
+}
 ?>
 
 <?php
-	//Controller
-	$authors = getAllauthors();
+//Controller
+$authors = getAllauthors();
 ?>
 
 <!--View-->
 <table border='1'>
-		<tr>
-			<td class="update"></td>
-			<td class="delete"></td>
-			<td class="id">ID</td>
-			<td class="title">Name</td>
-			<td class="author">Image</td>
-			<td class="catID">Desc</td>
-		</tr>
-		<tr>
-			<td class="update"></td>
-			<td class="delete"></td>
-			<td class="id"></td>
-			<td class="title"><a href="insert.php">Insert</a></td>
-			<td class="author"></td>
-			<td class="catID"></td>
-		</tr>
-	<?php foreach($authors as $author) :?>
-		<tr>
-			<td class="update"><a href="update.php?authorID=<?php echo $author["authorID"] ?>">Update</td>
-			<td class="delete"><a href="delete.php?authorID=<?php echo $author["authorID"] ?>">Delete</td>
-			<td class="id"><?php echo $author["authorID"]; ?> </td>
-			<td class="title"><a href="authorDetails.php?ID=<?php echo $author["authorID"] ?>"><?php echo $author["authorName"]; ?></a></td>
-			<td class="author"><?php echo $author["authorImage"]; ?> </td>
-			<td class="catID" style="max-width: 200px"><?php echo substr($author["authorDescription"], 0, 60). "..."; ?> </td>
-		</tr>
-	<?php endforeach ?>
+    <tr>
+        <td class="update"></td>
+        <td class="delete"></td>
+        <td class="id">ID</td>
+        <td class="title">Name</td>
+        <td class="author">Image</td>
+        <td class="catID">Desc</td>
+    </tr>
+    <tr>
+        <td class="update"></td>
+        <td class="delete"></td>
+        <td class="id"></td>
+        <td class="title"><a href="insert.php">Insert</a></td>
+        <td class="author"></td>
+        <td class="catID"></td>
+    </tr>
+    <?php foreach ($authors as $author) : ?>
+        <tr>
+            <td class="update"><a href="update.php?authorID=<?php echo $author["authorID"] ?>">Update</td>
+            <td class="delete"><a href="delete.php?authorID=<?php echo $author["authorID"] ?>">Delete</td>
+            <td class="id"><?php echo $author["authorID"]; ?> </td>
+            <td class="title"><a href="authorDetails.php?ID=<?php echo $author["authorID"] ?>"><?php echo $author["authorName"]; ?></a></td>
+            <td class="author"><?php echo $author["authorImage"]; ?> </td>
+            <td class="catID" style="max-width: 200px"><?php echo substr($author["authorDescription"], 0, 60) . "..."; ?> </td>
+        </tr>
+    <?php endforeach ?>
 </table>
